76b16196e23e7effb885615c7c52e1c18576a6ce
shr3y4sh <67783996+shr3y4sh@users.noreply.github.com>
2025-01-10 17:10:27 +0530

    change layout, add numberOfCells button

--- script.js
+++ script.js
@@ -1,20 +1,52 @@
-const container = document.querySelector("#main-container");
+const CONTAINER$SIZE = 700;
+let nCrossN;
 
-createGrid();
+// const nCrossN = 16;
 
 // let sketchLst = [];
 
-function createGrid() {
-  for (let i = 0; i < 16; i++) {
+const container = createGrid();
+function createGrid(nCrossN = 16) {
+  const container = document.querySelector("#main-container");
+  const cellWidth = CONTAINER$SIZE / nCrossN;
+  const cellHeight = CONTAINER$SIZE / nCrossN;
+
+  for (let i = 0; i < nCrossN; i++) {
     let row = document.createElement("div");
 
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < nCrossN; j++) {
       let cell = document.createElement("div");
       cell.classList.add("cell-style");
+      cell.style.width = cellWidth + "px";
+      cell.style.height = cellHeight + "px";
+      // cell.innerText = "x";
       row.appendChild(cell);
     }
     row.classList.add("row-style");
+    // row.style.height = `{800 / 16}px`;
     container.appendChild(row);
     // sketchLst.push(row);
   }
+
+  return container;
 }
+
+// createGrid();
+
+let sizeButton = document.querySelector("#size-button");
+
+sizeButton.addEventListener("click", function () {
+  document.querySelector("#main-container").remove();
+  let container = document.createElement("div");
+  container.setAttribute("id", "main-container");
+  document
+    .querySelector("#area")
+    .insertBefore(container, document.querySelector("#right-navigation"));
+  let num = prompt("Enter number of cells");
+  nCrossN = Number(num);
+  console.log("nCrossN: " + num);
+  container = createGrid(nCrossN);
+  console.log(container);
+});
+
+// createGrid(nCrossN);
